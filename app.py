19053996547a800af0c6668f4bdd3ac3d152248b
harith detbun <harith.detbun@gmail.com>
2022-03-06 00:40:47 +0700

    make it better

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import threading
 
 app = Flask(__name__)
 
-
 class GOTO_ROBOT(threading.Thread):
      
     def rb_dinammic(self,x,y,z,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19,A20,A21,A22):
