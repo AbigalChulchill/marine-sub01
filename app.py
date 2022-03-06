dea1b965fe19e441ce02ecde09de8cbccd5a644a
Marine INVEST <96211570+harith11000@users.noreply.github.com>
2022-03-06 22:46:43 +0700

    good

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ def rebalance(ENTER,x,y,z,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16
         
 
     else :
-        
         text = "STOP by error"
         print(text)
         GR.notifi(text,z)
