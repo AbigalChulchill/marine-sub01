1b8852db518dfa89151a0d82f40af59c97fa31ed
Marine INVEST <96211570+harith11000@users.noreply.github.com>
2022-03-13 14:46:29 +0700

    make it better

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def rebalance(ENTER,x,y,z,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16
     BOT_TRACK = threading.Thread(target=GR.robot_track, args=(x,y,z,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19,A20,dm,) , daemon=True)
     
     #---------------------------------------------------------------------------------------------
+    
     if ENTER == "T_SPOT":
 
         text = "T_SPOT"
