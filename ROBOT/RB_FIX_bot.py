6e27243de250ba7e0375d644245bb9622632b69f
Marine INVEST <96211570+harith11000@users.noreply.github.com>
2022-03-14 13:24:11 +0700

    make it better

--- ROBOT/RB_FIX_bot.py
+++ ROBOT/RB_FIX_bot.py
@@ -15,17 +15,9 @@ def rebalance_fix(api_key,api_secret,token,imail,ipass,remail,rec,begin_money,st
     
     print("get exchange")
     exchange = ccxt.binance  ({'apiKey' : api_key ,'secret' : api_secret ,'enableRateLimit': True})
-    Account_name = "Robot-Binance"
 
-    
-    exchange.apiKey = 'YOUR_BFX_API_KEY'
-    exchange.secret = 'YOUR_BFX_SECRET'
-
-    if Account_name == "" :
-        print("\n""Account Name - This is Main Account",': Broker - ',exchange)     
-    else:
-        print( "\n"'Account Name - ',Account_name,': Broker - ',exchange)
-        exchange.headers = {'Robot-Binance-SUBACCOUNT': Account_name}
+    # print('apiKey:', ccxt.api_Key)  # slightly edited these lines
+    # print('secret:', ccxt.api_secret)  # slightly edited these lines
 
     saving = Client(api_key,api_secret)
 
@@ -97,10 +89,11 @@ def rebalance_fix(api_key,api_secret,token,imail,ipass,remail,rec,begin_money,st
         signal = []
         print("EMA status")
         if EMAx == "Y":
-            timef = emat
+            
             print("EMA CHECK in")
-            print(exchange)
-            ohlcv = exchange.fetch_ohlcv(symbol = symbolx,timeframe=emat,limit=1000)
+            print('apiKey:', ccxt.apiKey)  # slightly edited these lines
+            print('secret:', ccxt.secret)  # slightly edited these lines
+            ohlcv = exchange.fetch_ohlcv(symbol = symbolx,timeframe=emat,limit=None)
             print("EMA CHECK out")
             data = pd.DataFrame(ohlcv, columns =['datetime', 'open','high','low','close','volume'])
             data['datetime']  = pd.to_datetime(data['datetime'], unit='ms')
